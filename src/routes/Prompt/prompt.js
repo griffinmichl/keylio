0a43f4b8c27e6b5d94a4bc6bb2c69a11bc249a5c
Griffin Michl <griffinmichl@gmail.com>
2016-05-31 20:54:33 -0700

    clear input field after each word

--- src/routes/Prompt/prompt.js
+++ src/routes/Prompt/prompt.js
@@ -1,9 +1,16 @@
 import Cycle from 'cycle-react'
+import keycode from 'keycode'
 
 const Prompt = Cycle.component('Prompt', function (interactions, props) {
+  const input$ = interactions.get('change')
+    .map(ev => ev.target.value)
+    .map(txt => txt.endsWith(' ') ? '' : txt)
+    .startWith('')
+
   const vtree$ = props
     .get('content')
-    .map(content =>
+    .combineLatest(input$)
+    .map(([content, input]) =>
       <div className="row">
         <div className="offset-3 col-6 prompt">
           <div className="prompt-text">
@@ -12,7 +19,9 @@ const Prompt = Cycle.component('Prompt', function (interactions, props) {
           <div className="prompt-input">
             <input
               type="text"
-              placeholder="type words here"
+              value={input}
+              placeholder="type word here"
+              onChange={interactions.listener('change')}
               onKeyDown={interactions.listener('keydown')}
               onKeyUp={interactions.listener('keyup')}
             />
--- src/routes/Prompt/view.js
+++ src/routes/Prompt/view.js
@@ -9,10 +9,10 @@ function view(state$, interactions) {
           <h3 className="col-12 sub-title">discover your unique typing style</h3>
         </header>
         <div>
-        {keyboard === null ? <Prompt content={prompt} keydown={interactions.listener('keydown')} keyup={interactions.listener('keyup')} /> : keyboard}
+          {keyboard === null ? <Prompt content={prompt} keydown={interactions.listener('keydown')} keyup={interactions.listener('keyup')} /> : keyboard}
         </div>
         <div>
-        {heatmap === null ? '' : heatmap}
+          {heatmap === null ? '' : heatmap}
         </div>
         <div className="row">
           <p className="offset-2 col-8 sub-title top-buffer">
